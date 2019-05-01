df38509a0fd4e4b3cdca451dc6aa53077b5b87d1
Chua Kang Ming <kambing860210@gmail.com>
2019-05-02 00:05:20 +0800

    improve performance

--- src/utils/fetchPackage.ts
+++ src/utils/fetchPackage.ts
@@ -3,7 +3,19 @@ import fetchJson from "./fetchJson";
 const registryUrl = "https://npm-registry-proxy.glitch.me/";
 // backup url
 // const registryUrl = "https://registry.npmjs.cf/";
+const registryCache: { [key: string]: Promise<any> } = {};
 
-export default (packageQuery: string) => {
-  return fetchJson(`${registryUrl}${packageQuery}`);
+export default async (packageQuery: string) => {
+  const cache = registryCache[packageQuery];
+  if (cache) {
+    return cache;
+  }
+  try {
+    const response = await (registryCache[packageQuery] = fetchJson(
+      `${registryUrl}${packageQuery}`
+    ));
+    return response;
+  } finally {
+    delete registryCache[packageQuery];
+  }
 };
