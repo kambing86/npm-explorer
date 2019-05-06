1885322677ab7941d4c271b6848d49da2b543d91
Chua Kang Ming <kambing860210@gmail.com>
2019-05-06 22:41:17 +0800

    add delay and cancel to auto complete input

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import AsyncCreatable from "react-select/lib/AsyncCreatable";
+import { Subject } from "rxjs";
 import { Button, Icon, Theme, withStyles } from "@material-ui/core";
-import { ValueType } from "react-select/lib/types";
+import CreatableSelect from "react-select/lib/Creatable";
+import { ValueType, InputActionMeta } from "react-select/lib/types";
+import { StateManager } from "react-select/lib/stateManager";
 import { isArray } from "lodash";
-
-import queryPackage from "./utils/queryPackage";
+import { queryPackage$ } from "./utils/queryPackage";
 import ConcurrencyInput from "./ConcurrencyInput";
 
 const styles = (theme: Theme) => ({
@@ -20,9 +21,15 @@ const styles = (theme: Theme) => ({
   }
 });
 
-export type OptionType = { label: string; value: string };
+export interface IOptionType {
+  label: string;
+  value: string;
+}
 
 interface ISearchState {
+  isLoading: boolean;
+  options: IOptionType[];
+  inputValue: string;
   value: string | null;
 }
 
@@ -35,10 +42,37 @@ class Search extends React.Component<ISearchProps, ISearchState> {
   constructor(props: ISearchProps) {
     super(props);
     this.state = {
+      isLoading: false,
+      options: [],
+      inputValue: "",
       value: null
     };
   }
-  changeValue = (input: ValueType<OptionType>) => {
+  reactSelectRef = React.createRef<StateManager<IOptionType>>();
+  onQuery = (data: any) => {
+    const allOptions: IOptionType[] = data.map((packageInfo: any) => ({
+      label: packageInfo.name,
+      value: packageInfo.name
+    }));
+    const { inputValue } = this.state;
+    const sortedOption = [
+      ...allOptions.filter(option => option.value === inputValue),
+      ...allOptions.filter(option => option.value !== inputValue)
+    ];
+    this.setState({
+      isLoading: false,
+      options: sortedOption
+    });
+  };
+  subject = new Subject<string>();
+  subscription = queryPackage$(this.subject).subscribe(this.onQuery);
+  inputChange = (value: string, event: InputActionMeta) => {
+    if (event.action === "input-change") {
+      this.setState({ isLoading: true, options: [], inputValue: value });
+      this.subject.next(value);
+    }
+  };
+  changeValue = (input: ValueType<IOptionType>) => {
     if (input) {
       if (isArray(input)) {
         this.setState({ value: input[0].value });
@@ -55,16 +89,35 @@ class Search extends React.Component<ISearchProps, ISearchState> {
       onClickSearch(this.state.value);
     }
   };
+  onInputKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.keyCode === 13) {
+      const stateManager = this.reactSelectRef.current;
+      if (stateManager && !stateManager.state.menuIsOpen) {
+        this.onClickSearch();
+        event.preventDefault();
+      }
+    }
+  };
+  componentWillUnmount = () => {
+    this.subscription.unsubscribe();
+  };
   render() {
     const { classes } = this.props;
+    const { isLoading, options } = this.state;
     return (
       <>
-        <AsyncCreatable
-          cacheOptions
-          defaultOptions
-          loadOptions={queryPackage}
+        <CreatableSelect
+          options={options}
+          isLoading={isLoading}
+          onInputChange={this.inputChange}
           onChange={this.changeValue}
           className={classes.reactSelect}
+          // remove default filterOption
+          filterOption={null}
+          createOptionPosition="first"
+          onKeyDown={this.onInputKeyDown}
+          // @ts-ignore
+          ref={this.reactSelectRef}
         />
         <Button
           variant="contained"
--- src/utils/queryPackage.ts
+++ src/utils/queryPackage.ts
@@ -1,15 +1,47 @@
 import fetchJson from "./fetchJson";
+import { EMPTY, Observable, Subject, of } from "rxjs";
+import { switchMap, delay, flatMap } from "rxjs/operators";
+
+const getQueryUrl = (query: string) =>
+  `https://npm-registry-proxy.glitch.me/search/suggestions?q=${query}`;
 
 export default async (query: string) => {
   if (query === "") {
     return [];
   }
 
-  const data = await fetchJson(
-    `https://npm-registry-proxy.glitch.me/search/suggestions?q=${query}`
+  return await fetchJson(getQueryUrl(query));
+};
+
+const getQueryObservable$ = (query: string) => {
+  return of(query).pipe(
+    delay(400),
+    flatMap(
+      query =>
+        new Observable<any>(subsriber => {
+          const abortController = new AbortController();
+          (async () => {
+            try {
+              const data = await fetchJson(
+                getQueryUrl(query),
+                abortController.signal
+              );
+              subsriber.next(data);
+              subsriber.complete();
+            } catch (e) {
+              subsriber.error(e);
+            }
+          })();
+          return () => {
+            abortController.abort();
+          };
+        })
+    )
   );
-  return data.map((packageInfo: any) => ({
-    label: packageInfo.name,
-    value: packageInfo.name
-  }));
+};
+
+export const queryPackage$ = (subject: Subject<string>) => {
+  return subject.pipe(
+    switchMap(query => (query === "" ? EMPTY : getQueryObservable$(query)))
+  );
 };
