f2e27dec2684c481d6a31c59d2e267bf6b268f15
Chua Kang Ming <kambing860210@gmail.com>
2019-11-29 10:56:19 +0800

    add 2 overloaded functions for useStateWithRef

--- src/hooks/useStateWithRef.ts
+++ src/hooks/useStateWithRef.ts
@@ -1,10 +1,23 @@
 import { useRef, useState } from "react";
 
-export const useStateWithRef = <T>(
-  initialValue: T,
-): [React.MutableRefObject<T>, React.Dispatch<React.SetStateAction<T>>] => {
+function useStateWithRef<S>(
+  initialState: S | (() => S),
+): [React.MutableRefObject<S>, React.Dispatch<React.SetStateAction<S>>];
+function useStateWithRef<S = undefined>(): [
+  React.MutableRefObject<S | undefined>,
+  React.Dispatch<React.SetStateAction<S | undefined>>,
+];
+
+function useStateWithRef<T>(
+  initialValue?: T | (() => T),
+): [
+  React.MutableRefObject<T | undefined>,
+  React.Dispatch<React.SetStateAction<T | undefined>>,
+] {
   const [state, setState] = useState(initialValue);
   const stateRef = useRef(state);
   stateRef.current = state;
   return [stateRef, setState];
-};
+}
+
+export { useStateWithRef };
