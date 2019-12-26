906d2df581920b3e6fd266f22ef4609a3c492331
Chua Kang Ming <kambing860210@gmail.com>
2019-12-26 17:35:21 +0800

    fix layout of search page

--- src/components/LoadPromise.tsx
+++ src/components/LoadPromise.tsx
@@ -5,15 +5,17 @@ import React, { Suspense } from "react";
 function LoadPromise() {
   const [data] = usePromiseWithSuspense(() => getAllVersionsAsync("react"));
   return (
-    <div className="flex-grow-1 d-flex flex-column justify-content-end align-self-end align-items-end">
+    <>
       <div>React</div>
       <div>latest version: {data.data?.latest}</div>
-    </div>
+    </>
   );
 }
 
 export default () => (
-  <Suspense fallback={<div>Loading</div>}>
-    <LoadPromise />
-  </Suspense>
+  <div className="flex-grow-1 d-flex flex-column justify-content-end align-self-end align-items-end">
+    <Suspense fallback={<>Loading</>}>
+      <LoadPromise />
+    </Suspense>
+  </div>
 );
