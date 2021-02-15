657e79adf94192cc8af0e6a961be14211ed72111
Chua Kang Ming <kambing860210@gmail.com>
2021-02-15 18:00:26 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import flatten from "flat";
+import jsonpath from "jsonpath";
 import React, { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import store from "./store";
 import theme from "./theme";
+
+const data = {
+  firstName: "John",
+  lastName: "doe",
+  age: 26,
+  address: {
+    streetAddress: "naist street",
+    city: "Nara",
+    postalCode: "630-0192",
+  },
+  phoneNumbers: [
+    {
+      type: "iPhone",
+      number: "0123-4567-8888",
+    },
+    {
+      type: "home",
+      number: "0123-4567-8910",
+    },
+  ],
+};
+
+console.log(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  flatten(data),
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  jsonpath.query(data, "$.phoneNumbers[1].type"),
+);
 
 const SearchComponent = lazy(() => import("./components/Search"));
 
