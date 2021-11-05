98412b24bff17f82ed4d54fd795e1798f0e8117b
Chua Kang Ming <kambing860210@gmail.com>
2021-11-05 12:17:17 +0800

    fix: lintstaged should be checking only

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": [
-    "prettier --write",
-    () => "yarn lint",
-  ],
+  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": ["prettier --check"],
+  "src/**/*.{js,jsx,ts,tsx}": ["eslint"],
 };
